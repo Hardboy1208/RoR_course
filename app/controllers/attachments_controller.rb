7f5264438db0277c3098924755506cf3cd826c43
Владимир <birbrover@gmail.com>
2018-04-09 18:51:32 +0500

    fix

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,9 +1,10 @@
 class AttachmentsController < ApplicationController
   def destroy
     attachment = Attachment.find(params[:id])
-    if attachment.destroy
-      redirect_to attachment.attachmentable.question if attachment.attachmentable.class.name == 'Answer'
-      redirect_to attachment.attachmentable if attachment.attachmentable.class.name == 'Question'
+    if current_user.author_of?(attachment.attachmentable)
+      if attachment.destroy
+        redirect_to attachment.question
+      end
     end
   end
 end
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,4 +2,8 @@ class Attachment < ApplicationRecord
   mount_uploader :file, FileUploader
 
   belongs_to :attachmentable, polymorphic: true, optional: true
+
+  def question
+    self.attachmentable.methods.include?(:question) ? self.attachmentable.question : self.attachmentable
+  end
 end
