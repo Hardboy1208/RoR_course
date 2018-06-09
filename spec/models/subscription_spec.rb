ed3954916ccaed5ce938b3cdf033e16709a89467
Владимир <birbrover@gmail.com>
2018-06-09 10:53:14 +0500

    fix

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,4 +3,15 @@ require "rails_helper"
 RSpec.describe Subscription, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:question) }
+
+  describe '.send_daily_digest' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:subscription) { create(:subscription, question: question, user: user) }
+
+    it 'should send daily digest to all users' do
+      expect(UserSubscriptionsMailer).to receive(:send_mail).with(user, question).and_call_original
+      Subscription.send_subscription
+    end
+  end
 end
