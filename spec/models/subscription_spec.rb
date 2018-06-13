a3aacdfde331d9b1f1d15ccc5bf8f58fdc0372e8
Владимир <birbrover@gmail.com>
2018-06-13 11:17:26 +0500

    fix

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Subscription, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:question) }
 
-  describe '.send_daily_digest' do
+  describe '.send_subscription' do
     let(:user) { create(:user) }
     let(:question) { create(:question, user: user) }
     let(:subscription) { create(:subscription, question: question, user: user) }
